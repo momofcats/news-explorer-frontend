caa3802ee0342e087edff60ec630ead8deb4f81f
Yuliya Khilko <ykhilko@gmail.com>
2020-12-03 14:41:35 -0800

    add fonts to the page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './components/App/App.css';
 import App from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
 
