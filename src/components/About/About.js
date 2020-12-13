84978f2163dc699a60827165b5ee7c7cc7d70655
Yuliya Khilko <ykhilko@gmail.com>
2020-12-12 18:47:47 -0800

    add profile image

---
+++ src/components/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+    return (
+        <section className="about">
+            <div className="about__img-container">
+                <img alt="Profile" className="about__img" src='../../images/profile-img.jpg' />
+            </div>
+        </section>
+    )
+}
+export default About;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,15 +2,19 @@ import React from "react";
 import "./Main.css";
 import Header from "../Header/Header";
 import SearchForm from '../SearchForm/SearchForm';
+import About from '../About/About';
 
 function Main() {
   return (
+    <>
     <section className="main">
       <div className="main__overlay">
         <Header />
         <SearchForm />
       </div>
     </section>
+    <About />
+    </>
   );
 }
 export default Main;
