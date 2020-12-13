9a4b2f604d9f3823e69d4bd0e140c1786f74c93c
Yuliya Khilko <ykhilko@gmail.com>
2020-12-12 16:17:10 -0800

    add onlick on burger

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,7 @@ function Header() {
     <header className={`header ${expanded ? "header_active" : ""}`}>
       <div className="header__wrapper">
         <div className="header__logo">NewsExplorer</div>
-        <button className="header__button header__button_type_closed" onClick={handleClick}></button>
+        <button className={`header__button ${expanded ? "header__button_type_expanded" : "header__button_type_closed"}`} onClick={handleClick}></button>
       </div>
       <Nav isOpen={expanded}/>
     </header>
