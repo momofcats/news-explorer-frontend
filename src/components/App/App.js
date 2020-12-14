30b5f4023b10970d77805fd69fe87b2a1b9fc84e
Yuliya Khilko <ykhilko@gmail.com>
2020-12-13 19:27:28 -0800

    create footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 import './App.css';
 import Main from '../Main/Main';
+import Footer from '../Footer/Footer';
 
 const App = () => {
     return (
         <>
         <Main />
+        <Footer />
         </>
     )
 };
