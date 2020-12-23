4befa0692e59f8966c8d3007065bc905a453a3f7
Yuliya Khilko <ykhilko@gmail.com>
2020-12-23 15:13:40 -0800

    add tooltip to markup

--- src/components/NewsCardList/NewsCardList.js
+++ src/components/NewsCardList/NewsCardList.js
@@ -49,6 +49,7 @@ function NewsCardsList() {
                 >
                   <path d="M11.382 15.714L6 19.942V4h12v15.942l-5.382-4.228-.618-.486-.618.486z" />
                 </svg>
+                <span className="card__tooltip-text">Sign in to save articles</span>
               </button>
             </div>
           </NewsCard>
--- src/components/SavedNews/SavedNews.js
+++ src/components/SavedNews/SavedNews.js
@@ -48,20 +48,22 @@ function SavedNews() {
         <NewsCard article={article} key={id}>
           <div className="card__buttons-wrapper card__buttons-wrapper_placement_spaceout">
             <p className="card__keyword">{article.keyword}</p>
-            <button type="button" className="card__button">
-              <svg
-                className="card__button-trash"
-                width="24"
-                height="24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M15 3H9v2H3v2h18V5h-6V3zM5 9v11a2 2 0 002 2h10a2 2 0 002-2V9h-2v11H7V9H5zm4 0v9h2V9H9zm4 0v9h2V9h-2z"
-                />
-              </svg>
-            </button>
+             
+              <button type="button" className="card__button">
+                <svg
+                  className="card__button-trash"
+                  width="24"
+                  height="24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M15 3H9v2H3v2h18V5h-6V3zM5 9v11a2 2 0 002 2h10a2 2 0 002-2V9h-2v11H7V9H5zm4 0v9h2V9H9zm4 0v9h2V9h-2z"
+                  />
+                </svg>
+                <span className="card__tooltip-text">Remove from saved</span>
+              </button>
           </div>
         </NewsCard>
       ))}
